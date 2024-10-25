60923a2d49a08841b32dfe9754c6ca0a0cae5ba1
Tayse Rosa <tayse_rosa@yahoo.com.br>
2024-10-25 16:43:37 -0300

    Rotas

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export const search = (req: Request, res:Response) => {
+    //res.render('pages/search')
+}
+
+export const home = (req: Request, res:Response) => {
+    //res.render('pages/home')
+}
+
+export const dogs = (req: Request, res:Response) => {
+    //res.render('pages/dogs')
+}
+
+export const cats = (req: Request, res:Response) => {
+    //res.render('pages/cats')
+}
+
+export const fishes = (req: Request, res:Response) => {
+    //res.render('pages/fishes')
+}
